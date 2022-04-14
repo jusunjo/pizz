684e80794930359884ee83c9a720525679f85364
SejuneOh <etry0715@gmail.com>
2022-04-14 19:33:56 +0900

    feat: Add read line Command And Fibonacci Numbers Function

---
+++ Fibonacci.js
@@ -0,0 +1,27 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+rl.on("line", (line) => {
+    console.log('입력받은 수: ' + line)
+    rl.close(); // 필수!! close가 없으면 입력을 무한히 받는다.
+});
+rl.on('close', () => {
+    // 입력이 끝난 후 실행할 코드
+        process.exit();
+})
+
+
+function Fibonacci_numbers(n){
+   if(n<=1){
+      return n
+   }
+   
+    return Fibonacci_numbers(n-1) + Fibonacci_numbers(n-2)
+
+}
+     
